e1a840533ea865f62ede76423ce5575c116df4b7
matteobsu <matteo.busi@psi.ch>
2022-06-01 14:59:27 +0200

    fix when data is odd length

--- functions/Reduction/wiener_deconvolution.m
+++ functions/Reduction/wiener_deconvolution.m
@@ -16,54 +16,88 @@ if(size(g,2)>1)
 end
 
 %% Correlation theorem
-F = fft(f); 
-G = fft(g); 
+F = fft(f);
+G = fft(g);
 
-arg = F.*conj(G)./((abs(G).^2)+c);  
+arg = F.*conj(G)./((abs(G).^2)+c);
 
 % complexplot(fftshift(arg));
 %%
 switch filter
-    case 'LowPass' 
-        x = 1:length(f);
-        x0 = length(x)/2+1;
-        width = round(x0/4);
-        K = zeros(length(F),1);
-        K(x0-round(width/2):x0+round(width/2))=1;
+    case 'LowPass'
+        lf = length(f);
+        x = 1:lf;
+        K = zeros(lf,1);
+        ww = 4;
+        if(mod(lf,2)==0)
+            x0 = length(x)/2+1;
+            width = round(x0/ww);
+            K(x0-round(width/2):x0+round(width/2))=1;
+        else
+            x0 = round(length(x)/2);
+            width = round(x0/ww);
+            K(x0-round(width/2):x0+round(width/2))=1;
+        end
         arg = fftshift(K).*arg;
-    case 'LowPassBu' 
-        x = 1:length(f);
-        x0 = length(x)/2+1;
-        width = round(x0/5);
+        
+    case 'LowPassBu'
+        lf = length(f);
+        x = 1:lf;
+        K = zeros(lf,1);
         n = 5;
-        K = zeros(length(F),1);
-        D = 1:1:(length(f)-x0);
-        HH = 1./(1+(D/width).^(2*n));
-        K(x0)=1;
-        K(x0+1:end)=HH;
-        D = 1:1:(x0-1);
-        HH = 1./(1+(D/width).^(2*n));
-        K(1:x0-1)=fliplr(HH);
+        ww = 5;
+        if(mod(lf,2)==0)
+            x0 = length(x)/2+1;
+            width = round(x0/ww);
+            K(x0)=1;
+            D = 1:1:(lf-x0);
+            HH = 1./(1+(D/width).^(2*n));
+            K(x0+1:end)=HH;
+            D = 1:1:(x0-1);
+            HH = 1./(1+(D/width).^(2*n));
+            K(1:x0-1)=fliplr(HH);
+        else
+            x0 = round(length(x)/2);
+            width = round(x0/ww);
+            K(x0)=1;
+            D = 1:1:(lf-x0);
+            HH = 1./(1+(D/width).^(2*n));
+            K(x0+1:end)=HH;
+            K(1:x0-1)=fliplr(HH);
+        end
+        
         arg = fftshift(K).*arg;
-    case 'LowPassGa' 
-        x = 1:length(f);
-        x0 = length(x)/2+1;
-        K = zeros(length(F),1);
-        D = 1:1:(length(f)-x0);
-        HH = exp(-(D.^2/(2*width^2)));
-        K(x0)=1;
-        K(x0+1:end)=HH;
-        D = 1:1:(x0-1);
-        HH = exp(-(D.^2/(2*width^2)));
-        K(1:x0-1)=fliplr(HH);
-
+        
+    case 'LowPassGa'
+        lf = length(f);
+        x = 1:lf;
+        K = zeros(lf,1);
+        if(mod(lf,2)==0)
+            x0 = length(x)/2+1;
+            width = round(x0/10);
+            K(x0)=1;
+            D = 1:1:(lf-x0);
+            HH = exp(-(D.^2/(2*width^2)));
+            K(x0+1:end)=HH;
+            D = 1:1:(x0-1);
+            HH = exp(-(D.^2/(2*width^2)));
+            K(1:x0-1)=fliplr(HH);
+        else
+            x0 = round(length(x)/2);
+            width = round(x0/10);
+            K(x0)=1;
+            D = 1:1:(lf-x0);
+            HH = exp(-(D.^2/(2*width^2)));
+            K(x0+1:end)=HH;
+            K(1:x0-1)=fliplr(HH);
+        end
+        
         arg = fftshift(K).*arg;
+        
     case 'none'
     otherwise
         disp('WARNING: Unrecognized filter, set to none')
 end
 
-
-H = ifft(arg);
+H = real(ifft(arg));
 end
-
