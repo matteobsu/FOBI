0b541fc3d605f854929fd179f830e381a16b4f57
matteobsu <matteo.busi@psi.ch>
2023-06-07 09:48:59 +0200

    speed performance

--- functions/Reduction/SpectrumRoi.m
+++ functions/Reduction/SpectrumRoi.m
@@ -1,20 +1,29 @@
-function [spectrum,unc] = SpectrumRoi(data,roi,method)
-%ROI_SPECTRUM Summary of this function goes here
-%   Detailed explanation goes here
-if exist('method','var') == 0
-    method='mean';
-end
-roi = double(roi);
-roi(roi==0)=nan;
-data = data.*repmat(roi,[1 1 size(data,3)]);
-for i=1:size(data,3)
-    a = data(:,:,i);
-    unc(i)=nanstd(a(:));
-    if(strcmp(method,'median')) %#ok<ALIGN>
-        spectrum(i)=nanmedian(a(:));
-    else
-    if(strcmp(method,'mean'))
-        spectrum(i)=nanmean(a(:));
+function [spectrum, unc] = SpectrumRoi(data, roi, method)
+    if nargin < 3
+        method = 'mean';
     end
-end
+    
+    roi = double(roi);
+    roi(roi == 0) = NaN;
+    
+    maskedData = bsxfun(@times, data, roi);
+    
+    numChannels = size(data, 3);
+    spectrum = zeros(numChannels, 1);
+    unc = zeros(numChannels, 1);
+    
+    for i = 1:numChannels
+        a = maskedData(:, :, i);
+        validElements = ~isnan(a(:));
+        
+        if strcmp(method, 'median')
+            spectrum(i) = median(a(validElements));
+        else
+            spectrum(i) = mean(a(validElements));
+        end
+        
+        if nargout==2
+        unc(i) = std(a(validElements));
+        end
+    end
 end
